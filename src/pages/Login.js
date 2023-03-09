58e6aae2fa05b751213e521a464a090f7b96b243
Carl Abel <caweb.cph@gmail.com>
2023-03-09 14:52:37 +0100

    login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,69 +3,71 @@ import { useState, useContext, useEffect } from "react"
 import { TokenContext } from "../components/TokenProvider"
 import { useNavigate } from "react-router-dom"
 import useCookie from "react-use-cookie"
+import { useForm } from "react-hook-form"
 
 export default function Login() {
-	const [isLoading, setIsLoading] = useState(false)
-	const [, setTokenCookie] = useCookie("trainer-cookie", undefined)
-	const { token, setToken } = useContext(TokenContext)
-	const navigate = useNavigate()
+  const [isLoading, setIsLoading] = useState(false)
+  const [, setTokenCookie] = useCookie("trainer-cookie", undefined)
+  const { token, setToken } = useContext(TokenContext)
+  const navigate = useNavigate()
+  const { register, handleSubmit, formState: { errors } } = useForm()
 
-	async function handleSubmit(event) {
-		event.preventDefault()
-		setIsLoading(true)
+  async function onSubmit(data) {
+    setIsLoading(true)
 
-		try {
-			const response = await axios.post("https://test-trainer-api.onrender.com/auth/token", {
-				username: event.target.username.value,
-				password: event.target.password.value
-			})
+    try {
+      const response = await axios.post("https://test-trainer-api.onrender.com/auth/token", {
+        username: data.username,
+        password: data.password
+      })
 
-			if (response.status === 200) {
-			if(event.target.remember.checked){
-				const milliseconds = response.data.validUntil - Date.now()
-				const validFor = milliseconds / (1000 * 60 * 60 * 24)
-				setTokenCookie(JSON.stringify(response.data), {
-					days: validFor,
-					SameSite: "Strict"
-				})
-			} else {
+      if (response.status === 200) {
+        if (data.remember) {
+          const milliseconds = response.data.validUntil - Date.now()
+          const validFor = milliseconds / (1000 * 60 * 60 * 24)
+          setTokenCookie(JSON.stringify(response.data), {
+            days: validFor,
+            SameSite: "Strict"
+          })
+        }
+        setToken(response.data)
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setIsLoading(false)
+    }
+  }
 
-			}
-				setToken(response.data)
-			}
-		} catch (error) {
-			
-		} finally {
-			setIsLoading(false)
-		}
-	}
+  useEffect(function() {
+    if (token) {
+      navigate("/profile")
+    }
+  }, [token, navigate])
 
-	useEffect(function() {
-		if (token) {
-			navigate("/profile")
-		}
-	}, [token, navigate])
-	
-	return (
-		<>
-			<h1>Log in</h1>
-			<form onSubmit={handleSubmit} style={{display:"flex", flexDirection:"column"}}>
-				<label>
-					Username
-					<input type="text" name="username" />
-				</label>
-				<label>
-					Password
-					<input type="password" name="password" />
-				</label>
-				<label>
-					<input type="checkbox" name="remember" />
-					Remember me
-				</label>
-				<button type="submit">Log in</button>
-				{isLoading && <p>Loading...</p>}
-			</form>
-
-		</>
-	)
+  return (
+    <>
+      <h1 className="text-2xl font-bold mb-4">Log in</h1>
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col">
+        <label className="mb-2">
+          Username
+          <input type="text" name="username" {...register("username", { required: true })} className={`border rounded py-1 px-2 ${errors.username && "border-red-500"}`} />
+          {errors.username && <span className="text-red-500">This field is required</span>}
+        </label>
+        <label className="mb-2">
+          Password
+          <input type="password" name="password" {...register("password", { required: true })} className={`border rounded py-1 px-2 ${errors.password && "border-red-500"}`} />
+          {errors.password && <span className="text-red-500">This field is required</span>}
+        </label>
+        <label className="mb-2">
+          <input type="checkbox" name="remember" {...register("remember")} />
+          Remember me
+        </label>
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" disabled={isLoading}>
+          {isLoading ? "Loading..." : "Log in"}
+        </button>
+      </form>
+    </>
+  )
 }
+
