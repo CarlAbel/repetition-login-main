affa6014781a423300fcca20cb12d72a421dafbf
Carl Abel <caweb.cph@gmail.com>
2023-04-17 16:50:03 +0200

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
     <div className="Home">
       <h1 className="text-center text-[28px] mb-8">Popular Class</h1>
       <PopularClass />
-      <h1 className="text-[28px] mb-4">Classes for you</h1>
+      <h1 className="text-[28px] mb-4">Classes For You</h1>
       <ClassesForYou />
     </div>
   )
